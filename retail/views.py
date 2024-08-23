f722563130a556b1997e185ccd63d118d3df8586
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-08-24 02:46:03 +0300

    Add permissions

--- retail/views.py
+++ retail/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from retail.models import RetailChain, Product
+from retail.serializers import RetailChainSerializer, ProductSerializer
+
+
+class RetailChainViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = RetailChainSerializer
+    queryset = RetailChain.objects.all()
+    filter_backends = [SearchFilter]
+
+    def perform_update(self, serializer):
+        if 'debt' in serializer.validated_data:
+            serializer.validated_data.pop('debt')
+            raise Exception("You can't update dept")
+        super().perform_update(serializer)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
